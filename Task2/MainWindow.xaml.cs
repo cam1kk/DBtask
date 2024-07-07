55a3a15e1e59a7c634f08aa1745cec1f3747ca1f
Babae_me80 <Babae_me80@STHQ0123-03>
2024-07-07 17:48:47 +0400

    Add project files.

---
+++ Task2/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace Task2
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            signUp.Show();
+        }
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Task2/SignUp.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Task2
+{
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = Name.Text;
+            string surname = SurName.Text;
+            string age = Age.Text;
+            string login = Login.Text;
+            string password = Password.Text;
+            string confirmPass = ConfirmPass.Text;
+            
+            if (!(name.Length == 0))
+            {
+                if(!(surname.Length == 0))
+                {
+
+                }
+            }
+        }
+    }
+}
